a5339f86d39cc3f4c75fecf1d0c5dd041fba18a9
Samet Alabey <samet.alabey@hotmail.com>
2018-12-12 13:58:47 +0300

    Added project folders

---
+++ HastaneOtomasyon/Forms/FrmDoktor.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace HastaneOtomasyon
+{
+    public partial class FrmDoktor : Form
+    {
+        public FrmDoktor()
+        {
+            InitializeComponent();
+        }
+    }
+}
