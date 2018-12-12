5ec6dc49d30d8c0ae7a480228e6016cbac542f9e
Samet Alabey <alabeysamet@gmail.com>
2018-12-12 21:41:18 +0300

    Randevu sınıfı oluşturuldu.

---
+++ HastaneOtomasyon/Concretes/Randevu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using HastaneOtomasyon.Abstracts;
+
+namespace HastaneOtomasyon.Concretes
+{
+    public class Randevu
+    {
+        private Doktor _doktor;
+        private Hasta _hasta;
+        private DateTime _tarih;
+        private bool _durum;
+
+        public Doktor Doktor
+        {
+            get => _doktor;
+            protected set => _doktor = value;
+        }
+
+        public Hasta Hasta
+        {
+            get => _hasta;
+            protected set => _hasta = value;
+        }
+
+        public DateTime Tarih
+        {
+            get => _tarih;
+            protected set => _tarih = value;
+        }
+
+        public bool Durum
+        {
+            get => _durum;
+            protected set => _durum = value;
+        }
+    }
+}
