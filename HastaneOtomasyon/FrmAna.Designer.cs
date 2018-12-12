bb6b240de0be1cd5119d47e598847e5f988f479b
Emre Çetin <emre_17_cetin@hotmail.com>
2018-12-12 13:54:51 +0300

    Formlar eklendi

---
+++ HastaneOtomasyon/FrmAna.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace HastaneOtomasyon
+{
+    partial class FrmAna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hastaIslemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doctorIslemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hemsireIslemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personelIslemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hastaIslemleriToolStripMenuItem,
+            this.doctorIslemleriToolStripMenuItem,
+            this.hemsireIslemleriToolStripMenuItem,
+            this.personelIslemleriToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hastaIslemleriToolStripMenuItem
+            // 
+            this.hastaIslemleriToolStripMenuItem.Name = "hastaIslemleriToolStripMenuItem";
+            this.hastaIslemleriToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.hastaIslemleriToolStripMenuItem.Text = "Hasta Islemleri";
+            this.hastaIslemleriToolStripMenuItem.Click += new System.EventHandler(this.hastaIslemleriToolStripMenuItem_Click);
+            // 
+            // doctorIslemleriToolStripMenuItem
+            // 
+            this.doctorIslemleriToolStripMenuItem.Name = "doctorIslemleriToolStripMenuItem";
+            this.doctorIslemleriToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.doctorIslemleriToolStripMenuItem.Text = "Doktor Islemleri";
+            this.doctorIslemleriToolStripMenuItem.Click += new System.EventHandler(this.doctorIslemleriToolStripMenuItem_Click);
+            // 
+            // hemsireIslemleriToolStripMenuItem
+            // 
+            this.hemsireIslemleriToolStripMenuItem.Name = "hemsireIslemleriToolStripMenuItem";
+            this.hemsireIslemleriToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.hemsireIslemleriToolStripMenuItem.Text = "Hemsire Islemleri";
+            this.hemsireIslemleriToolStripMenuItem.Click += new System.EventHandler(this.hemsireIslemleriToolStripMenuItem_Click);
+            // 
+            // personelIslemleriToolStripMenuItem
+            // 
+            this.personelIslemleriToolStripMenuItem.Name = "personelIslemleriToolStripMenuItem";
+            this.personelIslemleriToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.personelIslemleriToolStripMenuItem.Text = "Personel Islemleri";
+            this.personelIslemleriToolStripMenuItem.Click += new System.EventHandler(this.personelIslemleriToolStripMenuItem_Click);
+            // 
+            // FrmAna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmAna";
+            this.Text = "FrmAna";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hastaIslemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doctorIslemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hemsireIslemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personelIslemleriToolStripMenuItem;
+    }
+}
---
+++ HastaneOtomasyon/FrmAna.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HastaneOtomasyon
+{
+    public partial class FrmAna : Form
+    {
+        public FrmAna()
+        {
+            InitializeComponent();
+        }
+        private FrmDoktor _frmDoktor;
+        private FrmHasta _frmHasta;
+        private FrmHemsire _frmHemsire;
+        private FrmPersonel _frmPersonel;
+        private void hastaIslemleriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmHasta == null || _frmHasta.IsDisposed)
+            {
+                _frmHasta = new FrmHasta();
+                _frmHasta.WindowState = FormWindowState.Maximized;
+                _frmHasta.MdiParent = this;
+                _frmHasta.Show();
+            }
+        }
+
+        private void doctorIslemleriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmDoktor == null || _frmDoktor.IsDisposed)
+            {
+                _frmDoktor = new FrmDoktor();
+                _frmDoktor.WindowState = FormWindowState.Maximized;
+                _frmDoktor.MdiParent = this;
+                _frmDoktor.Show();
+            }
+        }
+
+        private void hemsireIslemleriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmHemsire == null || _frmHemsire.IsDisposed)
+            {
+                _frmHemsire = new FrmHemsire();
+                _frmHemsire.WindowState = FormWindowState.Maximized;
+                _frmHemsire.MdiParent = this;
+                _frmHemsire.Show();
+            }
+        }
+
+        private void personelIslemleriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmPersonel == null || _frmPersonel.IsDisposed)
+            {
+                _frmPersonel = new FrmPersonel();
+                _frmPersonel.WindowState = FormWindowState.Maximized;
+                _frmPersonel.MdiParent = this;
+                _frmPersonel.Show();
+            }
+        }
+    }
+}
