7980ec7f3a9c679edb85e570d311e89cc8f4b9f7
Samet Alabey <samet.alabey@hotmail.com>
2018-12-12 14:20:12 +0300

    Hasta sınıfı eklendi.

---
+++ HastaneOtomasyon/Concretes/Hasta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HastaneOtomasyon.Concretes
+{
+    public class Hasta
+    {
+        private string _ad, _soyad, _tcNo;
+        private DateTime _dogumTarihi;
+
+        public string Ad
+        {
+            get => _ad;
+            set => _ad = value;
+        }
+        public string Soyad
+        {
+            get => _soyad;
+            set => _soyad = value;
+        }
+
+        public string TcNo
+        {
+            get => _tcNo;
+            set => _tcNo = value;
+        }
+
+        public DateTime DogumTarihi
+        {
+            get => _dogumTarihi;
+            set => _dogumTarihi = value;
+        }
+
+
+    }
+}
