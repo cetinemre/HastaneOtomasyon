8d7ead4e5c14572a99787cda0da47aecdfd2d251
Samet Alabey <alabeysamet@gmail.com>
2018-12-13 20:28:09 +0300

    hh

--- HastaneOtomasyon/Forms/FrmDoktorEkle.cs
+++ HastaneOtomasyon/Forms/FrmDoktorEkle.cs
@@ -22,21 +22,6 @@ namespace HastaneOtomasyon.Forms
         {
             try
             {
-                if (cbBrans.SelectedIndex==0)
-                {
-                    Doktor yeniDoktor = new OrtopediDoktoru();
-                    yeniDoktor.Ad = txtAd.Text;
-                   
-                }
-                else if (cbBrans.SelectedIndex==1)
-                {
-                    Doktor yeniDoktor = new DisDoktoru();
-                }
-                else if (cbBrans.SelectedIndex == 2)
-                {
-                    Doktor yeniDoktor = new KbbDoktoru();
-                }
-               
             }
             catch (Exception ex)
             {
