7dc78752b661c38a6b376043f861ce3daf1990b2
Emre Çetin <emre_17_cetin@hotmail.com>
2018-12-13 10:38:25 +0300

    Görünürlük ayarları değiştirildi.

---
+++ HastaneOtomasyon/Forms/RandevuSaatleri.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace HastaneOtomasyon.Forms
+{
+    partial class RandevuSaatleri
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btns1 = new System.Windows.Forms.Button();
+            this.btns2 = new System.Windows.Forms.Button();
+            this.btns3 = new System.Windows.Forms.Button();
+            this.btns4 = new System.Windows.Forms.Button();
+            this.btns5 = new System.Windows.Forms.Button();
+            this.btns10 = new System.Windows.Forms.Button();
+            this.btns9 = new System.Windows.Forms.Button();
+            this.btns8 = new System.Windows.Forms.Button();
+            this.btns7 = new System.Windows.Forms.Button();
+            this.btns6 = new System.Windows.Forms.Button();
+            this.btns15 = new System.Windows.Forms.Button();
+            this.btns14 = new System.Windows.Forms.Button();
+            this.btns13 = new System.Windows.Forms.Button();
+            this.btns12 = new System.Windows.Forms.Button();
+            this.btns11 = new System.Windows.Forms.Button();
+            this.btns20 = new System.Windows.Forms.Button();
+            this.btns19 = new System.Windows.Forms.Button();
+            this.btns18 = new System.Windows.Forms.Button();
+            this.btns17 = new System.Windows.Forms.Button();
+            this.btns16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btns1
+            // 
+            this.btns1.Location = new System.Drawing.Point(3, 3);
+            this.btns1.Name = "btns1";
+            this.btns1.Size = new System.Drawing.Size(82, 25);
+            this.btns1.TabIndex = 0;
+            this.btns1.Text = "9.00";
+            this.btns1.UseVisualStyleBackColor = true;
+            // 
+            // btns2
+            // 
+            this.btns2.Location = new System.Drawing.Point(4, 33);
+            this.btns2.Name = "btns2";
+            this.btns2.Size = new System.Drawing.Size(82, 25);
+            this.btns2.TabIndex = 1;
+            this.btns2.Text = "9.15";
+            this.btns2.UseVisualStyleBackColor = true;
+            // 
+            // btns3
+            // 
+            this.btns3.Location = new System.Drawing.Point(3, 62);
+            this.btns3.Name = "btns3";
+            this.btns3.Size = new System.Drawing.Size(82, 25);
+            this.btns3.TabIndex = 2;
+            this.btns3.Text = "9.30";
+            this.btns3.UseVisualStyleBackColor = true;
+            // 
+            // btns4
+            // 
+            this.btns4.Location = new System.Drawing.Point(4, 91);
+            this.btns4.Name = "btns4";
+            this.btns4.Size = new System.Drawing.Size(82, 25);
+            this.btns4.TabIndex = 3;
+            this.btns4.Text = "9.45";
+            this.btns4.UseVisualStyleBackColor = true;
+            // 
+            // btns5
+            // 
+            this.btns5.Location = new System.Drawing.Point(4, 120);
+            this.btns5.Name = "btns5";
+            this.btns5.Size = new System.Drawing.Size(82, 25);
+            this.btns5.TabIndex = 4;
+            this.btns5.Text = "10.00";
+            this.btns5.UseVisualStyleBackColor = true;
+            // 
+            // btns10
+            // 
+            this.btns10.Location = new System.Drawing.Point(85, 120);
+            this.btns10.Name = "btns10";
+            this.btns10.Size = new System.Drawing.Size(82, 25);
+            this.btns10.TabIndex = 9;
+            this.btns10.Text = "11.15";
+            this.btns10.UseVisualStyleBackColor = true;
+            // 
+            // btns9
+            // 
+            this.btns9.Location = new System.Drawing.Point(85, 91);
+            this.btns9.Name = "btns9";
+            this.btns9.Size = new System.Drawing.Size(82, 25);
+            this.btns9.TabIndex = 8;
+            this.btns9.Text = "11.00";
+            this.btns9.UseVisualStyleBackColor = true;
+            // 
+            // btns8
+            // 
+            this.btns8.Location = new System.Drawing.Point(84, 62);
+            this.btns8.Name = "btns8";
+            this.btns8.Size = new System.Drawing.Size(82, 25);
+            this.btns8.TabIndex = 7;
+            this.btns8.Text = "10.45";
+            this.btns8.UseVisualStyleBackColor = true;
+            // 
+            // btns7
+            // 
+            this.btns7.Location = new System.Drawing.Point(85, 33);
+            this.btns7.Name = "btns7";
+            this.btns7.Size = new System.Drawing.Size(82, 25);
+            this.btns7.TabIndex = 6;
+            this.btns7.Text = "10.30";
+            this.btns7.UseVisualStyleBackColor = true;
+            // 
+            // btns6
+            // 
+            this.btns6.Location = new System.Drawing.Point(84, 3);
+            this.btns6.Name = "btns6";
+            this.btns6.Size = new System.Drawing.Size(82, 25);
+            this.btns6.TabIndex = 5;
+            this.btns6.Text = "10.15";
+            this.btns6.UseVisualStyleBackColor = true;
+            // 
+            // btns15
+            // 
+            this.btns15.Location = new System.Drawing.Point(166, 120);
+            this.btns15.Name = "btns15";
+            this.btns15.Size = new System.Drawing.Size(82, 25);
+            this.btns15.TabIndex = 14;
+            this.btns15.Text = "13.30";
+            this.btns15.UseVisualStyleBackColor = true;
+            // 
+            // btns14
+            // 
+            this.btns14.Location = new System.Drawing.Point(166, 91);
+            this.btns14.Name = "btns14";
+            this.btns14.Size = new System.Drawing.Size(82, 25);
+            this.btns14.TabIndex = 13;
+            this.btns14.Text = "13.15";
+            this.btns14.UseVisualStyleBackColor = true;
+            // 
+            // btns13
+            // 
+            this.btns13.Location = new System.Drawing.Point(165, 62);
+            this.btns13.Name = "btns13";
+            this.btns13.Size = new System.Drawing.Size(82, 25);
+            this.btns13.TabIndex = 12;
+            this.btns13.Text = "13.00";
+            this.btns13.UseVisualStyleBackColor = true;
+            // 
+            // btns12
+            // 
+            this.btns12.Location = new System.Drawing.Point(166, 33);
+            this.btns12.Name = "btns12";
+            this.btns12.Size = new System.Drawing.Size(82, 25);
+            this.btns12.TabIndex = 11;
+            this.btns12.Text = "11.45";
+            this.btns12.UseVisualStyleBackColor = true;
+            // 
+            // btns11
+            // 
+            this.btns11.Location = new System.Drawing.Point(165, 3);
+            this.btns11.Name = "btns11";
+            this.btns11.Size = new System.Drawing.Size(82, 25);
+            this.btns11.TabIndex = 10;
+            this.btns11.Text = "11.30";
+            this.btns11.UseVisualStyleBackColor = true;
+            // 
+            // btns20
+            // 
+            this.btns20.Location = new System.Drawing.Point(247, 120);
+            this.btns20.Name = "btns20";
+            this.btns20.Size = new System.Drawing.Size(82, 25);
+            this.btns20.TabIndex = 19;
+            this.btns20.Text = "14.45";
+            this.btns20.UseVisualStyleBackColor = true;
+            // 
+            // btns19
+            // 
+            this.btns19.Location = new System.Drawing.Point(247, 91);
+            this.btns19.Name = "btns19";
+            this.btns19.Size = new System.Drawing.Size(82, 25);
+            this.btns19.TabIndex = 18;
+            this.btns19.Text = "14.30";
+            this.btns19.UseVisualStyleBackColor = true;
+            // 
+            // btns18
+            // 
+            this.btns18.Location = new System.Drawing.Point(246, 62);
+            this.btns18.Name = "btns18";
+            this.btns18.Size = new System.Drawing.Size(82, 25);
+            this.btns18.TabIndex = 17;
+            this.btns18.Text = "14.15";
+            this.btns18.UseVisualStyleBackColor = true;
+            // 
+            // btns17
+            // 
+            this.btns17.Location = new System.Drawing.Point(247, 33);
+            this.btns17.Name = "btns17";
+            this.btns17.Size = new System.Drawing.Size(82, 25);
+            this.btns17.TabIndex = 16;
+            this.btns17.Text = "14.00";
+            this.btns17.UseVisualStyleBackColor = true;
+            // 
+            // btns16
+            // 
+            this.btns16.Location = new System.Drawing.Point(246, 3);
+            this.btns16.Name = "btns16";
+            this.btns16.Size = new System.Drawing.Size(82, 25);
+            this.btns16.TabIndex = 15;
+            this.btns16.Text = "13.45";
+            this.btns16.UseVisualStyleBackColor = true;
+            // 
+            // RandevuSaatleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btns20);
+            this.Controls.Add(this.btns19);
+            this.Controls.Add(this.btns18);
+            this.Controls.Add(this.btns17);
+            this.Controls.Add(this.btns16);
+            this.Controls.Add(this.btns15);
+            this.Controls.Add(this.btns14);
+            this.Controls.Add(this.btns13);
+            this.Controls.Add(this.btns12);
+            this.Controls.Add(this.btns11);
+            this.Controls.Add(this.btns10);
+            this.Controls.Add(this.btns9);
+            this.Controls.Add(this.btns8);
+            this.Controls.Add(this.btns7);
+            this.Controls.Add(this.btns6);
+            this.Controls.Add(this.btns5);
+            this.Controls.Add(this.btns4);
+            this.Controls.Add(this.btns3);
+            this.Controls.Add(this.btns2);
+            this.Controls.Add(this.btns1);
+            this.Name = "RandevuSaatleri";
+            this.Size = new System.Drawing.Size(345, 159);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btns1;
+        private System.Windows.Forms.Button btns2;
+        private System.Windows.Forms.Button btns3;
+        private System.Windows.Forms.Button btns4;
+        private System.Windows.Forms.Button btns5;
+        private System.Windows.Forms.Button btns10;
+        private System.Windows.Forms.Button btns9;
+        private System.Windows.Forms.Button btns8;
+        private System.Windows.Forms.Button btns7;
+        private System.Windows.Forms.Button btns6;
+        private System.Windows.Forms.Button btns15;
+        private System.Windows.Forms.Button btns14;
+        private System.Windows.Forms.Button btns13;
+        private System.Windows.Forms.Button btns12;
+        private System.Windows.Forms.Button btns11;
+        private System.Windows.Forms.Button btns20;
+        private System.Windows.Forms.Button btns19;
+        private System.Windows.Forms.Button btns18;
+        private System.Windows.Forms.Button btns17;
+        private System.Windows.Forms.Button btns16;
+    }
+}
