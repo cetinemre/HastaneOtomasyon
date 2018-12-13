13a4171d04272d5f8d4dc99da71cd73cc30bb199
Emre Çetin <emre_17_cetin@hotmail.com>
2018-12-13 11:19:26 +0300

    Form Boyutlandırılması Düzeltildi

--- HastaneOtomasyon/Forms/FrmDoktor.Designer.cs
+++ HastaneOtomasyon/Forms/FrmDoktor.Designer.cs
@@ -35,10 +35,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(804, 461);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "FrmDoktor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Doktor İşlemleri";
--- HastaneOtomasyon/Forms/FrmHasta.Designer.cs
+++ HastaneOtomasyon/Forms/FrmHasta.Designer.cs
@@ -212,8 +212,6 @@
             this.Controls.Add(this.gbDoktor);
             this.Controls.Add(this.gbServis);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "FrmHasta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hasta İşlemleri";
