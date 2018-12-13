17253037382c6e66bef17e49b5299ac1cfa96984
Emre Çetin <emre_17_cetin@hotmail.com>
2018-12-13 11:45:56 +0300

    ,,,

--- HastaneOtomasyon/Forms/FrmDoktor.Designer.cs
+++ HastaneOtomasyon/Forms/FrmDoktor.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDoktor));
             this.duzenleme1 = new HastaneOtomasyon.Forms.Duzenleme();
+            this.duzenleme2 = new HastaneOtomasyon.Forms.Duzenleme();
             this.SuspendLayout();
             // 
             // duzenleme1
@@ -39,12 +40,20 @@
             this.duzenleme1.Size = new System.Drawing.Size(571, 30);
             this.duzenleme1.TabIndex = 0;
             // 
+            // duzenleme2
+            // 
+            this.duzenleme2.Location = new System.Drawing.Point(-3, -1);
+            this.duzenleme2.Name = "duzenleme2";
+            this.duzenleme2.Size = new System.Drawing.Size(571, 30);
+            this.duzenleme2.TabIndex = 0;
+            // 
             // FrmDoktor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.duzenleme2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmDoktor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -56,5 +65,6 @@
         #endregion
 
         private Duzenleme duzenleme1;
+        private Duzenleme duzenleme2;
     }
 }
